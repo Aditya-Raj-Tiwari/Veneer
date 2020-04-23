9f6c5792d89d718435af3ee15686b282e7ee9827
ArtRajTiwari <artrajtiwari@gmail.com>
2020-04-23 22:43:56 +0200

    add Marketplace class

--- veneer.py
+++ veneer.py
@@ -4,11 +4,27 @@ class Art:
         self.title = title
         self.year = year
         self.medium = medium
-
     def __repr__(self):
-        return self.artist + "," + self.title + "," + self.year + "," + self.medium
+        return '%s. "%s". %s. "%s".' % (self.artist, self.title, self.year, self.medium)
+
+class Marketplace:
+    def __init__(self):
+        self.listings = []
+    def add_listing(self,new_listing):
+        self.listings.append(new_listing)
+    def remove_listings(self,expired_listing):
+        self.listings.remove(expired_listing)
+    def show_listingsI(self):
+        for items in self.listings:
+            print(items)
 
 
-girl_with_mandolin = Art('Picasso, Pablo','Girl with a Mandolin (Fanny Tellier)','1910','oil in canvas')
+
+
+
+girl_with_mandolin = Art('Picasso, Pablo', 'Girl with a Mandolin (Fanny Tellier)', '1910', 'oil in canvas')
 
 print(girl_with_mandolin)
+
+vaneer = Marketplace()
+print(vaneer.show_listingsI())
