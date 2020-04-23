7592e4bc8ce1603394f1eb896ccc1267e8d43ecd
ArtRajTiwari <artrajtiwari@gmail.com>
2020-04-23 22:57:21 +0200

    add owner details: name and location is Art class repr

--- veneer.py
+++ veneer.py
@@ -1,11 +1,12 @@
 class Art:
-    def __init__(self, artist, title, year, medium):
+    def __init__(self, artist, title, year, medium,owner):
         self.artist = artist
         self.title = title
         self.year = year
         self.medium = medium
+        self.owner = owner
     def __repr__(self):
-        return '%s. "%s". %s. "%s".' % (self.artist, self.title, self.year, self.medium)
+        return '%s. "%s". %s. "%s". "%s" , "%s". ' % (self.artist, self.title, self.year, self.medium,self.owner.name,self.owner.location)
 
 class Marketplace:
     def __init__(self):
@@ -28,12 +29,14 @@ class Client:
 
 
 
-girl_with_mandolin = Art('Picasso, Pablo', 'Girl with a Mandolin (Fanny Tellier)', '1910', 'oil in canvas')
 
-print(girl_with_mandolin)
 
 vaneer = Marketplace()
 print(vaneer.show_listings())
 
 edytta = Client("Edytta Halpirt","Private Collection", is_museum=False)
 moma = Client("The MOMA", "New York", is_museum=True)
+
+girl_with_mandolin = Art('Picasso, Pablo', 'Girl with a Mandolin (Fanny Tellier)', '1910', 'oil in canvas',moma)
+print(girl_with_mandolin)
+
